4706afd726a0d7d37a5b9261966e5f0b9756493d
kerwin <dear.kerwin@gmail.com>
2013-03-17 14:49:21 +0800

    fix bug

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ require_once("message_factory.php");
 require_once("core.php");
 
 // if(Safe::vaild()) {
-	$post_str = $GLOBALS["HTTP_RAW_POST_DATA"];
-	$post_str = "<xml>
+	$template = "<xml>
        <ToUserName><![CDATA[toUser]]></ToUserName>
        <FromUserName><![CDATA[fromUser]]></FromUserName>
        <CreateTime>1348831860</CreateTime>
        <MsgType><![CDATA[text]]></MsgType>
        <Content><![CDATA[detail]]></Content>
      </xml>";
+	$post_str = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS["HTTP_RAW_POST_DATA"] :$template;
+	
 	$msgObj = MessageFactory::init($post_str);
 	$respone_str  = $msgObj->get_msg();
 	echo $respone_str;
