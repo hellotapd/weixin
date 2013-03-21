c5df2c3f06c70f731bab56d335961a515631b1d0
weixin <377418118@qq.com>
2013-03-21 15:53:13 +0300

    change a verson

--- debug/post_message.php
+++ debug/post_message.php
@@ -1,19 +1,23 @@
 <?php
-//send mesage to index then return the response
-class SendMessage {
-	const $ip = 'weixin.ifxoxo.com';
-	const $port = 10086;
-	var $timeout = 10;
-	function __construct($timeout) {
-		$this->timeout = $timeout;
-	}
-
-	function send_message($str) {
-		$fp = fsockopen(self::ip, self::port, 0, '', $this->timeout);
-		if(!$fp) {
-			return false;
-		}
+/*
+ *send message to index then return the response
+ */
+class SendMessage{
+		const url = 'weixin.ifxoxo.com/index.php';
+		var $timeout = 10;
+		var $method = array('POST','GET');
 		
-	}
+		public function send_message($str) {
+			$ch        =    curl_init();
+			curl_setopt($ch,CURLOPT_URL,self::url);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $str);
+
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//...........
+			$curl_result = curl_exec($ch);
+			echo '<br>';
+			echo 'result is :'.$curl_result;
+		}
+
+
 }
-?>
