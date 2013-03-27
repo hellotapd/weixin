5de9a2509d0ffc4bc6b10ad1ce36b3b507f1728b
kerwin <dear.kerwin@gmail.com>
2013-03-27 13:56:49 +0800

    更新之后自动覆盖core。php

--- debug/lib/update.sh
+++ debug/lib/update.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/expect -f
 
 spawn su weixin
-#spawn su 
 expect "Password"
 sleep 0.5
 send "weixin@tapD\r"
-#send "Akerwin1021\r"
 expect "$"
 sleep 0.5
 send "cd /var/www/weixin\r"
 expect "$"
 send "/usr/local/git/bin/git pull master 2>&1\r"
 expect "$"
-sleep 2
+sleep 1
+send "cp core_default.php core.php"
+expect "$"
+sleep 0.5
 exit
