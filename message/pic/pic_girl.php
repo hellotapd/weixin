efe616a2820c0ce0c0ed9922033090bc03793958
kerwin <dear.kerwin@gmail.com>
2013-04-02 13:22:45 +0800

    增加pic test

--- message/pic/pic_girl.php
+++ message/pic/pic_girl.php
@@ -6,18 +6,18 @@ class PicGirl extends PicMessage{
 
 	function get_content() {
 		$content = array();
-		for ($i=0; $i < 4; $i++) { 
-			$content[] = $this->_get_one_item();	
+		for ($i=1; $i < 4; $i++) { 
+			$content[] = $this->_get_one_item("test".$i."jpg");	
 		}
 		return $content;	
 	}
 
-	function _get_one_item() {
+	function _get_one_item($pic_name) {
 		return array(
 				'title' => " beautiful girl",
 				'descript' => " this is a beautiful girl, you may like it",
-				'pic_url' => "http://pic.pare.cn/p3/2013-03-25-16-12-40_fw580",
-				'url' => "http://mp.weixin.qq.com/wiki/index.php?title=%E8%87%AA%E5%AE%9A%E4%B9%89%E8%8F%9C%E5%8D%95%E6%8E%A5%E5%8F%A3"
+				'pic_url' => BASE_PATH."static".DS."pic".DS.$pic_name,
+				'url' =>  BASE_PATH."static".DS."pic".DS.$pic_name
 			);
 	}
 
