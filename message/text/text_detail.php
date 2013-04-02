ff7cdacef7a8266c72e279bb081177d99cb3d8ad
kerwin <dear.kerwin@gmail.com>
2013-04-02 19:41:58 +0800

    尝试增加链接

--- message/text/text_detail.php
+++ message/text/text_detail.php
@@ -8,7 +8,8 @@ class TextDetail extends TextMessage{
 		$current_user = $this->current_user();
 		$system_user = $this->system_user();
 		$keyword = $this->keyword();
-		$content = "current_user:[{$current_user}]; system_user:[{$system_user}];keyword:[{$keyword}]";
+		$link = "<a href='".BASE_PATH."static".DS."pic".DS."test1.jpg'>查看美女</a>";
+		$content = " hello ~~~ this is detail ~~ 点击 ".$link;
 		return $content;
 	}
 
