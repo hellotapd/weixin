c7e913d5fbfa8812b357f6f9732244dae2c05fc9
hellotapd <377418118@qq.com>
2013-04-09 16:40:12 +0800

    fix bug

--- message/message_factory.php
+++ message/message_factory.php
@@ -3,10 +3,10 @@
 require_once("lib/keywords/SplitWords.php");
 class MessageFactory{
 	static function init($post_data) {
-		$postObj = simplexml_load_string($post_data, 'SimpleXMLElement', LIBXML_NOCDATA);
-        //$keyword = trim($postObj->Content);
+	$postObj = simplexml_load_string($post_data, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $keyword = trim($postObj->Content);
         $split = new SplitWords();
-        $keyword = $split->RunRMM($postObj->Content);
+        $keyword = $split->RunRMM($keyword);
         $keyword = implode('; ', $keyword);
         $clas_name = self::_get_class_name($keyword);
         loadMessage($clas_name);
