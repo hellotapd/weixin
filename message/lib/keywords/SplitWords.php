42cbcd7f66e113d9427eac09252931a3c6d3eace
andycwang <351250730@qq.com>
2013-04-10 17:01:23 +0800

    fenci

--- message/lib/keywords/SplitWords.php
+++ message/lib/keywords/SplitWords.php
@@ -1,215 +1,92 @@
-<?php
+﻿<?php
 class SplitWords{
- var $TagDic = Array();
- var $RankDic = Array();
- var $SourceStr = '';
- var $ResultStr = '';
- var $SplitChar = ' '; //
- var $SplitLen = 4;  //.....
- var $MaxLen = 7;  //.......................
- var $MinLen = 3;  //.....................
+	var $MaxLen = 3;			//字典里字符串的最大长度
+	var $MinLen = 2;			//最小长度
+	var $MatchMaxFirst = true;	//是否优先匹配最长字符串 如“心电图”和“心电”，true的时候匹配出心电图，false的时候匹配出心电
+	var $dic = array();			//字典
 
-  function SplitWord(){
-   $this->__construct();
-  }
-  
-  function __construct(){      
-   //..................
-   $dicfile = dirname(__FILE__)."/ppldic.csv"; 
-   $fp = fopen($dicfile,'r');   //.......
-   while($line = fgets($fp,256)){
-      $ws = explode(' ',$line);  //..........
-      $this->TagDic[$ws[0]] = $ws[1];
-      $this->RankDic[strlen($ws[0])][$ws[0]] = $ws[2];
-   }
-   fclose($fp);  //......
-  }
-  
-  //....
- function Clear(){
-   @fclose($this->QuickDic);
-  }
-  
-  //......
-  function SetSource($str){
-   $this->SourceStr = $this->UpdateStr($str);
-   $this->ResultStr = "";
-  }
-  
-  //............
-  function NotGBK($str)
-  {
-    if($str=="") return "";
-   if( ord($str[0])>0x80 ) return false;
-   else return true;
-  }
 
-  //......
-  function SplitRMM($str=""){
-   if($str!="") $this->SetSource($str);
-   if($this->SourceStr=="") return "";
-   $this->SourceStr = $this->UpdateStr($this->SourceStr);
-   $spwords = explode(" ",$this->SourceStr);
-   $spLen = count($spwords);
-   $spc = $this->SplitChar;
-   for($i=($spLen-1);$i>=0;$i--){
-    if($spwords[$i]=="") continue;
-    if($this->NotGBK($spwords[$i])){
-  	if(ereg("[^0-9\.\+\-]",$spwords[$i]))
-  	{ $this->ResultStr = $spwords[$i].$spc.".".$this->ResultStr; 
-  	}
-  	else
-  	{
-  	$nextword = "";
-  	@$nextword = substr($this->ResultStr,0,strpos($this->ResultStr,""));
-  	}
-    }
-    else
-    {
-      $c = $spwords[$i][0].$spwords[$i][1];
-      $n = hexdec(bin2hex($c));
-       if(strlen($spwords[$i]) <= $this->SplitLen)
-       {
-       }
-       else
-       {
-        $this->ResultStr = $this->RunRMM($spwords[$i]).$spc.$this->ResultStr;
-  	
-       }
-     }
-   }
-  // echo strlen($this->ResultStr)."<br>";
-  // .............
-    $this->ResultStr = substr($this->ResultStr,0,strlen($this->ResultStr)-3);
-   return $this->ResultStr;
-  }
-  //RMM.... .................
-  function RunRMM($str){
-   $spLen = strlen($str);
-   $rsStr = "";
-   $okWord = "";
-   $tmpWord = "";
-   $WordArray = Array();
-   
-   //......
-   for($i=($spLen-1);$i>=0;){
-    //.i..........
-    if($i<=$this->MinLen){
-     if($i==1){
-       $WordArray[] = substr($str,0,2);
-      }else
-     {
-        $w = substr($str,0,$this->MinLen+1);
-  	//echo "$w is :".$w."<br>";
-        if($this->IsWord($w)){
-         $WordArray[] = $w;                                                                                  
-        }else{
-         //$WordArray[] = substr($str,2,2);
-         //$WordArray[] = substr($str,0,2);
-        }
-      }
-     $i = -1; break;
-    }
-    //............
-    if($i>=$this->MaxLen) $maxPos = $this->MaxLen;
-    else $maxPos = $i;
-    $isMatch = false;
-  
-    for($j=$maxPos;$j>=0;$j=$j-2){
-      $w = substr($str,$i-$j,$j+1);
-      if($this->IsWord($w)){
-       $WordArray[] = $w;
-       $i = $i-$j-1;
-       $isMatch = true;
-       break;
-      }
-    }
-  if($isMatch == false){
-  	//echo "false $i<br>";
-  	$i -= 2;
-  }
-   }
-   $rsStr = $this->otherword($WordArray);
-   return explode(' ',$rsStr);
-  }
-  
-/** ............................
-**/
-function otherword($WordArray){
+	/**
+	 * 将字符串拆成一个字符一个值的数组
+	 * 
+	 * @author sunson
+	 */
+	function _deal_str_to_arr($str) {
+		//中文字符的正则表达式
+		$pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";
+		preg_match_all($pa, $str, $t_string);
+		return $t_string[0];
+	}
 
-   $wlen = count($WordArray)-1;      //.........
-   //echo "$wlen<br>";
-   $rsStr = $WordArray[$wlen];          //.....
-   $spc = $this->SplitChar;
-   for($i=$wlen-1;$i>=0;$i--)
-   {
-   $rsStr .= $spc.$WordArray[$i];   //..........
-   }
-   //........
-   // ........
-   $rsStr = preg_replace("/^".$spc."/","",$rsStr);
-  
-   return $rsStr;
-  }
-  
-  //............
-  function IsWord($okWord){
-   $slen = strlen($okWord);
-   if($slen > $this->MaxLen) return false;
-   else return isset($this->RankDic[$slen][$okWord]);
-  }
-  
-  //.......................
-  function UpdateStr($str){
-   $spc = $this->SplitChar;
-    $slen = strlen($str);
-    if($slen==0) return '';
-    $okstr = '';
-    $prechar = 0; // 0-.. 1-.. 2-.. 3-..
-    for($i=0;$i<$slen;$i++){
-      if(ord($str[$i]) < 0x81){
-        //.......
-        if(ord($str[$i]) < 33){
-          if($prechar!=0&&$str[$i]!="\r"&&$str[$i]!="\n") $okstr .= $spc;
-          $prechar=0;
-          continue; 
-        }else if(ereg("[^0-9a-zA-Z@\.%#:/\\&_-]",$str[$i])){
-          if($prechar==0){ $okstr .= $str[$i]; $prechar=3;}
-          else{ $okstr .= $spc.$str[$i]; $prechar=3;}
-        }else{
-         if($prechar==2||$prechar==3)
-         { $okstr .= $spc.$str[$i]; $prechar=1;}
-         else
-         { 
-           if(ereg("@#%:",$str[$i])){ $okstr .= $str[$i]; $prechar=3; }
-           else { $okstr .= $str[$i]; $prechar=1; }
-         }
-        }
-      }
-      else{
-        //......................
-        if($prechar!=0 && $prechar!=2) $okstr .= $spc;
-        //......
-        if(isset($str[$i+1])){
-          $c = $str[$i].$str[$i+1];
-          
-          $n = hexdec(bin2hex($c));
-          if($n<0xA13F && $n > 0xAA40){
-             if($prechar!=0) $okstr .= $spc.$c;
-             else $okstr .= $c;
-             $prechar = 3; 
-            }
-          else{
-            $okstr .= $c;
-            $prechar = 2;
-          }
-          $i++;
-        }
-      }
-    }
-    return $okstr;
-  }
+	/**
+	 * 根据开始和结束获取数组组成的字符串
+	 * 
+	 * @author sunson
+	 */
+	function _get_str_from_arr($arr, $begin, $end) {
+		$ret = '';
+		for($i = $begin; $i <= $end; $i++) {
+			$ret .= $arr[$i];
+		}
+		return $ret;
+	}
+
+	/**
+	 * 获取可用来检查的字符串数组
+	 * 
+	 * @author sunson
+	 */
+	function _get_arr_tobe_checked($i, $str_arr) {
+		$ret = array();
+		if($i < $this->MinLen) {
+			return array(implode('', $str_arr));
+		}
+		$begin = $i > $this->MaxLen ? $i - $this->MaxLen : 0;
+		for(; $begin <= $i - $this->MinLen; $begin ++) {
+			$ret []= $this->_get_str_from_arr($str_arr, $begin, $i -1);
+		}
+		return $ret;
+	}
+
+	/**
+	 * 真正用来匹配的逻辑
+	 * 
+	 * @author sunson
+	 */
+	function _do_check($check_arr) {
+		if(!$this->MatchMaxFirst) {
+			$check_arr = array_reverse($check_arr);
+		}
+		foreach ($check_arr as $key => $value) {
+			if(isset($this->dic[$value])) {
+				$tem_value = $this->_deal_str_to_arr($value);
+				return array('key' => $value, 'val' => $this->dic[$value], 'length' => count($tem_value));
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 用RMM算法来查找关键字
+	 * 
+	 * @author sunson
+	 */
+	function split($str) {
+		$str = $this->_deal_str_to_arr($str);
+		$key_word = array();
+		for($i = count($str); $i >0; ) {
+			$check_arr = $this->_get_arr_tobe_checked($i, $str);
+			$is_checked = $this->_do_check($check_arr);
+			if($is_checked !== false) {
+				$key_word []= $is_checked;
+				$i -= $is_checked['length'];
+			}else {
+				$i --;
+			}
+		}
+		return $key_word;
+	}
+
+
 }
 ?>
- 
-
--- message/lib/keywords/text.php
+++ message/lib/keywords/text.php
@@ -1,13 +1,15 @@
-<?php
+﻿<?php
 require_once("SplitWords.php");
+require_once("dictionary.php");
 
 $split = new SplitWords();
-$okWord = "切分技术参考指定and++两边重点讲解,今天天气怎么样？";
-  echo "开始<br>";
-  //echo "粗分词结果：".$split->UpdateStr($okWord)."<br>";
-  //$myWord = $split->UpdateStr($okWord);
-  //echo "提取关键词 SplitRMM：".$split->SplitRMM($myWord)."<br>";
-  echo "Run RMM :".$split->RunRMM($okWord);
-  $a = $split->RunRMM($okWord);
-  var_dump($a);
+$okWord = "决";
+$split->dic = $dic;
+$split->MaxLen = 3;
+$split->MinLen = 2;
+// $split->MatchMaxFirst = false;
+$a = $split->split($okWord);
+echo "the return is :<br>";
+print_r($a);
+
 ?>
