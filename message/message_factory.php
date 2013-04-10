57959cf73799812949a194b797f7aeb61dc86b12
andycwang <351250730@qq.com>
2013-04-10 18:38:04 +0800

    message/message_factory.php

--- message/message_factory.php
+++ message/message_factory.php
@@ -1,17 +1,23 @@
 <?php
 // require_once("../core.php");
-//require_once("lib/keywords/SplitWords.php");
+require_once("lib/keywords/SplitWords.php");
+require_once("lib/keywords/dictionary.php");
 class MessageFactory{
-	static function init($post_data) {
-	$postObj = simplexml_load_string($post_data, 'SimpleXMLElement', LIBXML_NOCDATA);
+    static function init($post_data) {
+        global $dic;
+        $postObj = simplexml_load_string($post_data, 'SimpleXMLElement', LIBXML_NOCDATA);
         $keyword = trim($postObj->Content);
         $split = new SplitWords();
-        $keywords = $split->split($keyword);
+        $split->dic = $dic;
+        $split->MaxLen = 5;
+        $split->MinLen = 2;
+        $keywords = $split->split((string)$keyword);
         debug($keywords);
         $clas_name = self::_get_class_name($keyword);
         loadMessage($clas_name);
         return new $clas_name($postObj);
-	}
+        }
+    }
 
 
 	static function _get_class_name($keyword) {
